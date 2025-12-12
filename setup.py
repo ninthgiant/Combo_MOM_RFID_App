474d44d7eb4ab195601955011a16105bc1371668
ninthgiant <mauckr@kenyon.edu>
2025-12-12 10:52:20 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+APP = ['COMBO_Viewer_v01b.py']
+
+DATA_FILES = []
+
+OPTIONS = {
+    'argv_emulation': True,
+    'packages': ['pandas', 'numpy'],
+    'includes': ['cmath', 'math'],   # ← FIX ADDED HERE
+    'plist': {
+        'CFBundleName': 'COMBO Viewer',
+        'CFBundleDisplayName': 'COMBO Viewer',
+        'CFBundleIdentifier': 'com.bobmauck.combo.viewer',
+        'CFBundleVersion': '1.0',
+        'CFBundleShortVersionString': '1.0',
+    },
+}
+
+setup(
+    app=APP,
+    data_files=DATA_FILES,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'],
+)
